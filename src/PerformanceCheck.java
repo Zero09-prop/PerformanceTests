0cc381bc2626e0b1cea75802e7851e2428cae0f4
Zero09-prop <72509842+Zero09-prop@users.noreply.github.com>
2021-03-29 15:07:10 +0300

    Add JavaDoc

--- src/PerformanceCheck.java
+++ src/PerformanceCheck.java
@@ -38,6 +38,14 @@ public class PerformanceCheck {
 		ClearTimeTest(list);
 	}
 	
+	/***
+	 * Method that counts the performance of add operations
+	 * List.Add(Integer e)
+	 * List.Add(int index,Integer element)
+	 * List.addAll(Collection<? extends Integer> c)
+	 * List.addAll(int index, Collection<? extends Integer> c)
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void AddOperationTimeTest(List<Integer> collection) {
 		
 		long begin = System.nanoTime();
@@ -64,6 +72,13 @@ public class PerformanceCheck {
 		end = System.nanoTime();
 		System.out.println("'addAll(int index, Collection <? extends E> collection)' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/***
+	 * Method that counts the performance of delete operations
+	 * List.remove(int index)
+	 * List.removeAll(Collection<?> c)
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void RemoveOperationTimeTest(List<Integer> collection) {
 
 		
@@ -82,6 +97,12 @@ public class PerformanceCheck {
 		
 		System.out.println("'removeAll(Collection<?> c)' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/**
+	 * Method that counts the performance of sorting operations
+	 * List.sort(Comparator<? super Integer> c)
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void SortOperationTimeTest(List<Integer> collection) {
 
 			
@@ -91,6 +112,12 @@ public class PerformanceCheck {
 			
 			System.out.println("'sort(Comparator<? super E> c)' operation: " + (double)(end - begin)/1000000 + " ms");
 		}
+	
+	/***
+	 * Method that counts the performance of size() operations
+	 * List.size()
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void GetSizeTimeTest(List<Integer> collection) {
 		
 		long begin = System.nanoTime();
@@ -99,6 +126,13 @@ public class PerformanceCheck {
 		
 		System.out.println("'size()' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/**
+	 * Method that counts the performance of contains() operations
+	 * List.contains(Object o)
+	 * List.containsAll(Collection<?> c)
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void ContainsTimeTest(List<Integer> collection) {
 		
 		long begin = System.nanoTime();
@@ -115,6 +149,12 @@ public class PerformanceCheck {
 		
 		System.out.println("'containsAll(Collection<?> c)' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/**
+	 * Method that counts the performance of isEmpty() operations
+	 * List.isEmpty()
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void IsEmptyTimeTest(List<Integer> collection) {
 		
 		long begin = System.nanoTime();
@@ -122,6 +162,13 @@ public class PerformanceCheck {
 		long end = System.nanoTime();
 		System.out.println("'isEmpty()' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/**
+	 * Method that counts the performance of IndexOf() operations
+	 * List.indexOf(Object o)
+	 * List.lastIndexOf(Object o)
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void IndexOfTimeTest(List<Integer> collection) {
 		
 		long begin = System.nanoTime();
@@ -135,6 +182,12 @@ public class PerformanceCheck {
 		
 		System.out.println("'lastIndexOf(Object o)' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/**
+	 * Method that counts the performance of get() operations
+	 * List.get(int index)
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void GetTimeTest(List<Integer> collection) {
 		
 		Iterator<Integer> iter = collection.iterator();
@@ -146,12 +199,24 @@ public class PerformanceCheck {
 		
 		System.out.println("'Get' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/**
+	 * Method that counts the performance of ToArray() operations
+	 * List.toArray()
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void ToArrayTimeTest(List<Integer> collection) {
 		long begin = System.nanoTime();
 		collection.toArray();
 		long end = System.nanoTime();
 		System.out.println("'ToArray' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/**
+	 * Method that counts the performance of replaceAll() operations
+	 * List.replaceAll(UnaryOperator<Integer> operator)
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void ReplaceAllTimeTest(List<Integer> collection) {
 		
 		long begin = System.nanoTime();
@@ -160,6 +225,12 @@ public class PerformanceCheck {
 		
 		System.out.println("'replaceAll(UnaryOperator<Integer> operator)' operation: " + (double)(end - begin)/1000000 + " ms");
 	}
+	
+	/**
+	 * Method that counts the performance of clear() operations
+	 * List.clear()
+	 * @param collection - ArrayList or LinkedList
+	 */
 	public static void ClearTimeTest(List<Integer> collection) {
 		
 		long begin = System.nanoTime();
